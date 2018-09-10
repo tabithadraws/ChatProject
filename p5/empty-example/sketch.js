827c62e5ba354db3139349c4c04e90887aba5b71
tabithadraws <43122173+tabithadraws@users.noreply.github.com>
2018-09-10 15:42:56 -0400

    added empty p5 example

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,26 @@
+var input, button, greeting;
+
+function setup() {
+
+  // create canvas
+  createCanvas(710, 400);
+
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'what is your name?');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function greet() {
+  var name = input.value();
+  greeting.html('hello '+name+'!');
+  input.value('');
+}
