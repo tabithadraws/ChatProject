4baffed99b0f047e7b4a544749064bd99077c007
tabithadraws <43122173+tabithadraws@users.noreply.github.com>
2018-09-11 20:11:25 -0400

    Trying to add a second button!
    It didn't work because I'm probably pasting things in the wrong spot!

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -24,3 +24,17 @@ function greet() {
   greeting.html('You would like to '+name+'!');
   input.value('');
 }
+
+var button;
+function setup() {
+  createCanvas(100, 100);
+  background(0);
+  button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(changeBG);
+}
+
+function changeBG() {
+  var val = random(255);
+  background(val);
+}
